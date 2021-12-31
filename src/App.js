cd81cda84c35ca658302e169441b5310e8021ef8
aymanab5 <aymansce@gmail.com>
2021-12-31 13:37:00 +0200

    add some routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import "./App.css";
-import Nav from "./Components/Nav";
-function App() {
-  return (
-    <div className="App">
-      <Nav />
-    </div>
-  );
+import React, { Component } from "react";
+import Nav from "./components/Nav";
+import Landing from "./components/Landing";
+import Catalog from "./components/Catalog";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/catalog" element={<Catalog />} />
+        </Routes>
+      </Router>
+    );
+  }
 }
 
 export default App;
